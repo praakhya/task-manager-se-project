5300c67faf51961c2ead951beb3e53612cf2db03
praakhya <67508200+praakhya@users.noreply.github.com>
2023-10-08 14:50:43 +0530

    Working get in ToDoList Component

--- se-project/src/ToDoList.js
+++ se-project/src/ToDoList.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
 import './ToDo.css'
+import ToDoItem from './ToDoItem';
 
-
+{/* <ToDoItem
+            title="First"
+            description="This is the first item" />,
+          <ToDoItem
+            title="Second"
+            description="This is the second item" />,
+          <ToDoItem
+            title="Third"
+            description="This is the third item" />,
+        ]} */}
 class ToDoList extends Component {
     constructor(props) {
         super();
-        this.list=[]
+        this.state = {list:[]}
     }
     getToDo() {
         console.log("in get ToDo");
@@ -22,22 +32,48 @@ class ToDoList extends Component {
             if (response.status != 200) {
               throw new Error(response.message);
             }
+            console.log("immediated response: ",response)
             return response.json()
           })
           .then((response) => {
             console.log("response in getToDo: ", response);
             console.log("data: ", response);
-            this.list = response;
+            this.setItemList(response)            
           })
           .catch((err) => {
             console.log(err);
           });
       };
+    setItemList(response) {
+      var temp = []
+      for (let todo of response) {
+        temp.push({
+          "title":todo.title,
+          "description":todo.description,
+          "done":todo.done
+        })
+      }
+      this.setState({list:temp});
+      console.log("list: ",this.state.list)
+    }
+    componentDidMount() {
+      console.log("before getToDo")
+      this.getToDo()
+      console.log("ToDoList mounted")
+    }
     render() {
+        var itemList = []
+        for (let todo of this.state.list){
+          itemList.push(<ToDoItem title={todo.title} description={todo.description} done={todo.done}/>)
+        }
+        console.log("itemList: ",itemList)
         return (
+          [
             <div className="ToDoList">
-            {this.list}
-            </div>
+            {itemList}
+            </div>,
+            <p>ToDoList</p>
+          ]
         );
     }
   }
